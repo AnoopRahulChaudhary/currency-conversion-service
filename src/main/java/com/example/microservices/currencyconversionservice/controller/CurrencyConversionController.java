3d91f65b4e28f56aab92b9285c8705ede1f52337
Anoop Chaudhary <anooprahulchaudhary@gmail.com>
2023-10-08 00:26:28 +0530

    Added controller for currency conversion

---
+++ src/main/java/com/example/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -0,0 +1,18 @@
+package com.example.microservices.currencyconversionservice.controller;
+
+import com.example.microservices.currencyconversionservice.model.CurrencyConversion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion convertCurrency(@PathVariable String from, @PathVariable String to,
+                                              @PathVariable BigDecimal quantity) {
+        return new CurrencyConversion(1000, from, to, BigDecimal.ONE, quantity, BigDecimal.ONE, "");
+    }
+}
---
+++ src/main/java/com/example/microservices/currencyconversionservice/model/CurrencyConversion.java
@@ -0,0 +1,24 @@
+package com.example.microservices.currencyconversionservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyConversion {
+
+    private int id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+
+}
